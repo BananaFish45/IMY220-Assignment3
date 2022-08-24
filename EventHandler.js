5c43bd29178f1204ccf61a4b5cf67627c8d8133d
Paul Nhlapo <paulnhlapo4@gmail.com>
2022-08-24 10:33:14 +0200

    Done everything works now

--- EventHandler.js
+++ EventHandler.js
@@ -1,4 +1,12 @@
 // import {events } from './script';
+var events = [
+	{name: "University expo", description: "Expo to showcase University degrees", dateStart: "2022/02/01", dateEnd: "2022/02/14"},
+    {name: "Market", description: "Farmer's market day long event", dateStart: "2022/06/12", dateEnd: "2022/06/12"},
+    {name: "Science Expo", description: "Science expo with sciency stuff", dateStart: "2022/06/12", dateEnd: "2022/06/21"},
+    {name: "Hiking trip", description: "Hiking trip with a bunch of University friends", dateStart: "2022/02/14", dateEnd: "2022/02/16"},
+    {name: "Music festival", description: "Weekend long music festival with a ton of artists performing", dateStart: "2022/05/13", dateEnd: "2022/05/15"},
+    {name: "Park Picnic", description: "Picnic event in the park", dateStart: "2022/06/12", dateEnd: "2022/06/12"}
+];
 
 function EventHandler(events) {
   this.events = events;
@@ -11,59 +19,54 @@ function EventHandler(events) {
   };
 
   this.getByMonth = function (month) {
-    return events.filter(item=> item.dateStart.substring(5,6)=month);
+    return events.filter(item=> item.dateStart.substring(5,7)==month);
   };
   
   this.getUniqueDateAndSort = function () {
-    events.sort((a, b) => {
-      return a.dateStart.split("/")[1] - b.dateStart.split("/")[1]
-    })
-      .filter((CurrentDay, index, CurrentDate) => CurrentDate.findIndex(
-          Today=>Today.dateStart === CurrentDay.dateStart === index
-    ));
+    return events.sort((a, b) => 
+      a.dateStart.split("/")[1] - b.dateStart.split("/")[1]
+    )
+      .filter((CurrentDay, index, today_date) => today_date.findIndex(
+          (Today)=>Today.dateStart === CurrentDay.dateStart )=== index
+    );
+
   };
   
 
 
   this.getSummary = function () {
     let eventSummary = [];
-    // if (typeof arguments === Object)
-    // {
-    //   eventSummary = Array.prototype.slice.apply(arguments); 
-    // }
-    // else if (arguments.length > 0) {
-    //   eventSummary = arguments[0];
-    // }
-    // else if (arguments.length == 0)
-    // {
-    //   eventSummary = this.events;
-    // }
-    // else if (this.constructor === Array)
-    // {
-      
-    // }
-    switch(arguments){
-    case arguments.length>0:
-        eventSummary = arguements[0];
-    case arguments.length==0:
-        eventSummary = this.events;
-    case typeof argument === Object:
-        eventSummary = Array.prototype.slice.appy(arguments);
-    default: 
-        eventSummary = this;
+    if (typeof arguments === Object) {
+      eventSummary = Array.prototype.slice.apply(arguments);
     }
+    else if (arguments.length > 0) {
+      eventSummary = arguments[0];
+    }
+    else if (arguments.length == 0) {
+      eventSummary = this.events;
+    }
+    else if (this.constructor === Array) {
+      eventSummary = this.Array;
+    }
+    else {
 
-    return eventSummary.map((eventday)=>{
-      if (eventday.dateStart == eventday.dateEnd)
-      {
-        return "On " + eventday.dateStart + " : " + eventday.name + " (" + eventday.description + ")";
-      }
-      else {
-        return "From" + eventday.dateStart + " to " + eventday.dateEnd + " :  " + eventday.name + " (" + eventday.description + ")";
-      }
+      eventSummary = arguments[0];
+    }
+      
+    return eventSummary.map((summary)=>{
+        if(summary.dateStart == summary.dateEnd)
+        {
+            return "On " + summary.dateStart + ": " + summary.name + " (" + summary.description + ")";
+        }
+        else {
+            return "From " + summary.dateStart + " to " + summary.dateEnd + ": " + summary.name + " (" + summary.description + ")";
+        }
     });
-    
+
   };
+  
+  
+    
 
 };
 
@@ -74,42 +77,56 @@ Array.prototype.getEventsBetweenDates = function (dateStart, dateEnd) {
 }
 
  Array.prototype.getByMonth = function (month) {
-    return events.filter(item=> item.dateStart.substring(5,6)=month);
+    return events.filter(item=> item.dateStart.substring(5,7)==month);
 }
 
 Array.prototype.getUniqueDateAndSort = function () {
-    events.sort((a, b) => {
-      return a.dateStart.split("/")[1] - b.dateStart.split("/")[1]
-    })
-      .filter((CurrentDay, index, CurrentDate) => CurrentDate.findIndex(
-          Today=>Today.dateStart === CurrentDay.dateStart === index
-    ));
+   return events.sort((a, b) => 
+      a.dateStart.split("/")[1] - b.dateStart.split("/")[1]
+    )
+      .filter((CurrentDay, index, today_date) => today_date.findIndex(
+          (Today)=>Today.dateStart === CurrentDay.dateStart )=== index
+    );
+
 }
   
 Array.prototype.getSummary = function ()
 {
-    let eventSummary = [];
-
-   switch(arguments){
-    case arguments.length>0:
-        eventSummary = arguements[0];
-    case arguments.length==0:
-        eventSummary = this.events;
-    case typeof argument === Object:
-        eventSummary = Array.prototype.slice.appy(arguments);
-    default: 
-        eventSummary = this;
+      let eventSummary = [];
+    if (typeof arguments === Object) {
+      eventSummary = Array.prototype.slice.apply(arguments);
     }
-  console.log(eventSummary);
+    else if (arguments.length > 0) {
+      eventSummary = arguments[0];
+    }
+    else if (arguments.length == 0) {
+      eventSummary = this.events;
+    }
+    else if (this.constructor === Array) {
+      eventSummary = this.Array;
+    }
+    else {
 
-    return eventSummary.map((eventday)=>{
-      if (eventday.dateStart == eventday.dateEnd)
-      {
-        return "On " + eventday.dateStart + " : " + eventday.name + " (" + eventday.description + ")";
-      }
-      else {
-        return "From" + eventday.dateStart + " to " + eventday.dateEnd + " :  " + eventday.name + " (" + eventday.description + ")";
-      }
+      eventSummary = arguments[0];
+    }
+      
+    return eventSummary.map((summary)=>{
+        if(summary.dateStart == summary.dateEnd)
+        {
+          return "On " + summary.dateStart + ": " + summary.name + " (" + summary.description + ")";
+        }
+        else
+        {
+          return "From " + summary.dateStart + " to " + summary.dateEnd + ": " + summary.name + " (" + summary.description + ")";
+        }
     });
-  
 }
+
+// let handler = new EventHandler(events);
+
+// let m = handler.getByMonth(05);
+// let m = handler.getEventsBetweenDates("2022/02/01", "2022/02/16");
+// let m = handler.getUniqueDateAndSort();
+// let m = handler.getSummary();
+
+// console.log(m);
